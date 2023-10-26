32af64274b66694c7de6c415575c56b45ec5dd64
Magdalena Ślusarczyk <91876400+shionorix@users.noreply.github.com>
2023-10-26 16:53:24 +0200

    added encode and decode methods

--- gui.py
+++ gui.py
@@ -9,10 +9,19 @@ class MainWindow(QMainWindow):
         super(MainWindow,self).__init__()
         loadUi("gui.ui",self)
         self.browseBtn.clicked.connect(self.browsefiles)
+        self.encodeBtn.clicked.connect(self.encode)
+        self.decodeBtn.clicked.connect(self.decode)
 
-    def browsefiles(self):
+    def browsefiles(self): # browse files and set file path to 'self.filename' edit
         fname=QFileDialog.getOpenFileName(self, 'Open file', os.getcwd(), 'Text files (*.txt, *.doc, *.docx)')
         self.filename.setText(fname[0])
+
+    def encode(self): # encoding supplied text
+        userText = self.userInput.toPlainText()
+        print(userText)
+
+    def decode(self): # decoding uploaded textfile
+        print(self.filename.text())
 
 if __name__ == "__main__":
     app=QApplication(sys.argv)
